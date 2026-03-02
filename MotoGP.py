e63f1a7e5d1fbcaa80d617ee7f1ca1f3c723e5e0
jan438 <jan438@users.noreply.github.com>
2026-03-02 09:35:22 +0100

    motogp added

---
+++ MotoGP.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import csv
+import math
+import unicodedata
+from pathlib import Path
+from datetime import datetime, date, timedelta
+from reportlab.pdfgen import canvas
+from reportlab.lib.colors import HexColor
+from reportlab.lib.units import inch, mm
+
+if sys.platform[0] == 'l':
+    path = '/home/jan/git/MotoGP'
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/MotoGP"
+os.chdir(path)
+my_canvas = canvas.Canvas("PDF/MotoGP.pdf")
+my_canvas.setFillColor(HexColor('#FECDE5'))
+p = my_canvas.beginPath()
+p.arc(2.0, 12.0, 22.0, 32.0, startAng = 90, extent = 90)
+my_canvas.drawPath(p, fill=1, stroke=1)
+my_canvas.save()
+key = input("Wait")
