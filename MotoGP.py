0a19a87f270dbd4ef86abe464f058796e857551f
jan438 <jan438@users.noreply.github.com>
2026-03-04 14:46:12 +0100

    to 15

--- MotoGP.py
+++ MotoGP.py
@@ -51,7 +51,7 @@ drawing = scaleSVG('SVG/MotoGPlogo.svg', 0.1)
 renderPDF.draw(drawing, my_canvas, 50, 750)
 
 for i in range(len(circuitsdata)):
-    if i == 13:
+    if i == 15:
         break
     if i == 11:
         col = 4  
