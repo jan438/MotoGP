fad0cce61e65e49f43cba80988cbd61976ab73ba
jan438 <jan438@users.noreply.github.com>
2026-03-04 14:13:27 +0100

    cze added

--- MotoGP.py
+++ MotoGP.py
@@ -51,7 +51,7 @@ drawing = scaleSVG('SVG/MotoGPlogo.svg', 0.1)
 renderPDF.draw(drawing, my_canvas, 50, 750)
 
 for i in range(len(circuitsdata)):
-    if i == 5:
+    if i == 6:
         break
     scale = float(circuitsdata[i][1])
     drawing = scaleSVG("SVG/" + circuitsdata[i][0] + ".svg", scale)
@@ -61,65 +61,6 @@ for i in range(len(circuitsdata)):
     if col == 5:
         col = 0
         circuit_y = circuit_y - rowheight
-
-drawing = scaleSVG('SVG/Indonesia.svg', 0.5)
-renderPDF.draw(drawing, my_canvas, 50, 50)
-my_canvas.drawString(50, 45, "Indonesia")
-drawing = scaleSVG('SVG/Italy.svg', 0.5)
-renderPDF.draw(drawing, my_canvas, 150, 50)
-my_canvas.drawString(150, 45, "Italy")
-
-drawing = scaleSVG('SVG/Thailand.svg', 0.5)
-renderPDF.draw(drawing, my_canvas, 450, 50)
-my_canvas.drawString(450, 45, "Thailand")
-
-drawing = scaleSVG('SVG/USA.svg', 0.1)
-renderPDF.draw(drawing, my_canvas, 150, 250)
-my_canvas.drawString(150, 245, "USA")
-drawing = scaleSVG('SVG/Qatar.svg', 0.09)
-renderPDF.draw(drawing, my_canvas, 250, 250)
-my_canvas.drawString(250, 245, "Qatar")
-drawing = scaleSVG('SVG/Spain.svg', 0.5)
-renderPDF.draw(drawing, my_canvas, 350, 250)
-my_canvas.drawString(350, 245, "Spain")
-drawing = scaleSVG('SVG/France.svg', 0.1)
-renderPDF.draw(drawing, my_canvas, 450, 250)
-my_canvas.drawString(450, 245, "France")
-
-drawing = scaleSVG('SVG/Hungary.svg', 0.1)
-renderPDF.draw(drawing, my_canvas, 50, 450)
-my_canvas.drawString(50, 445, "Hungary")
-drawing = scaleSVG('SVG/Czechia.svg', 0.1)
-renderPDF.draw(drawing, my_canvas, 150, 450)
-my_canvas.drawString(150, 445, "Czechia")
-drawing = scaleSVG('SVG/Netherlands.svg', 0.5)
-renderPDF.draw(drawing, my_canvas, 250, 450)
-my_canvas.drawString(250, 445, "Netherlands")
-drawing = scaleSVG('SVG/Germany.svg', 0.5)
-renderPDF.draw(drawing, my_canvas, 350, 450)
-my_canvas.drawString(350, 445, "Germany")
-drawing = scaleSVG('SVG/Great Britain.svg', 0.1)
-renderPDF.draw(drawing, my_canvas, 450, 450)
-my_canvas.drawString(450, 445, "Great Britain")
-
-
-drawing = scaleSVG('SVG/San Marino;.svg', 0.5)
-renderPDF.draw(drawing, my_canvas, 150, 650)
-my_canvas.drawString(150, 645, "San Marino")
-
-drawing = scaleSVG('SVG/Japan.svg', 0.4)
-renderPDF.draw(drawing, my_canvas, 350, 650)
-my_canvas.drawString(350, 645, "Japan")
-drawing = scaleSVG('SVG/Malaysia.svg', 0.5)
-renderPDF.draw(drawing, my_canvas, 450, 650)
-my_canvas.drawString(450, 645, "Malaysia")
-
-drawing = scaleSVG('SVG/Portugal.svg', 0.4)
-renderPDF.draw(drawing, my_canvas, 50, 350)
-my_canvas.drawString(50, 345, "Portugal")
-drawing = scaleSVG('SVG/Valencia.svg', 0.5)
-renderPDF.draw(drawing, my_canvas, 450, 350)
-my_canvas.drawString(450, 345, "Valencia")
 
 my_canvas.save()
 key = input("Wait")
