0615b7b06c135a799b09216e842448f561e11197
jan438 <jan438@users.noreply.github.com>
2026-03-13 16:01:42 +0100

    hand added

--- ParseSVGPath.py
+++ ParseSVGPath.py
@@ -2,6 +2,8 @@
 from svgpathtools import parse_path, Path, Line, CubicBezier
 from cmath import rect
 from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.colors import red, green
 
 def line_to_cubic(line: Line) -> CubicBezier:
     """
@@ -14,6 +16,50 @@ def line_to_cubic(line: Line) -> CubicBezier:
     ctrl1 = start + (end - start) / 3
     ctrl2 = start + 2 * (end - start) / 3
     return CubicBezier(start, ctrl1, ctrl2, end)
+    
+def hand(c, debug=1, fill=0):
+    (startx, starty) = (0,0)
+    curves = [
+    ( 0, 2), ( 0, 4), ( 0, 8), # back of hand
+    ( 5, 8), ( 7,10), ( 7,14),
+    (10,14), (10,13), ( 7.5, 8), # thumb
+    (13, 8), (14, 8), (17, 8),
+    (19, 8), (19, 6), (17, 6),
+    (15, 6), (13, 6), (11, 6), # index, pointing
+    (12, 6), (13, 6), (14, 6),
+    (16, 6), (16, 4), (14, 4),
+    (13, 4), (12, 4), (11, 4), # middle
+    (11.5, 4), (12, 4), (13, 4),
+    (15, 4), (15, 2), (13, 2),
+    (12.5, 2), (11.5, 2), (11, 2), # ring
+    (11.5, 2), (12, 2), (12.5, 2),
+    (14, 2), (14, 0), (12.5, 0),
+    (10, 0), (8, 0), (6, 0), # pinky, then close
+    ]
+
+    if debug: 
+        c.setLineWidth(6)
+    u = inch*0.2
+    p = c.beginPath()
+    p.moveTo(startx, starty)
+    ccopy = list(curves)
+    while ccopy:
+        [(x1,y1), (x2,y2), (x3,y3)] = ccopy[:3]
+        del ccopy[:3]
+        p.curveTo(x1*u,y1*u,x2*u,y2*u,x3*u,y3*u)
+    p.close()
+    c.drawPath(p, fill=fill)
+    if debug:
+        (lastx, lasty) = (startx, starty)
+        ccopy = list(curves)
+        while ccopy:
+            [(x1,y1), (x2,y2), (x3,y3)] = ccopy[:3]
+            del ccopy[:3]
+            c.setStrokeColor(red)
+            c.line(lastx*u,lasty*u, x1*u,y1*u)
+            c.setStrokeColor(green)
+            c.line(x2*u,y2*u, x3*u,y3*u)
+            (lastx,lasty) = (x3,y3)
     
 def hello(c):
     c.drawString(100,100,"Hello World")
@@ -34,7 +80,8 @@ print("Original path:", svg_path_str)
 print("Converted path:", new_path.d())
 
 c = canvas.Canvas("PDF/hello.pdf")
-hello(c)
+#hello(c)
+hand(c, 1, 0)
 c.showPage()
 c.save()
 
