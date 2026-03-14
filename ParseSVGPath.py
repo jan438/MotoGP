b5e80e28ac004ca5eb14051ca13c1eec91a61303
jan438 <jan438@users.noreply.github.com>
2026-03-14 13:40:10 +0100

    break added

--- ParseSVGPath.py
+++ ParseSVGPath.py
@@ -94,6 +94,7 @@ for path_string in path_strings:
             control2 = cubic.control2
             end = cubic.end
             print("cubic", start, control1, control2, end)
+            break
 p.moveTo(start_x, start_y)
 p.curveTo(start_x + 1, start_y + 1 ,start_x + 10, start_y + 40, start_x + 18, start_y + 28)
 p.close()
