4e33d7d70a3c6c1f5423f25f47718b98c1b43589
jan438 <jan438@users.noreply.github.com>
2026-03-16 11:34:09 +0100

    braziladded

--- ParseSVGPath.py
+++ ParseSVGPath.py
@@ -42,7 +42,7 @@ for segment in path:
     else:
         converted_segments.append(segment)
 c = canvas.Canvas("PDF/Path.pdf")
-doc = minidom.parse("Austriav.svg")
+doc = minidom.parse("Brazilv.svg")
 path_strings = [path.getAttribute('d') for path
                 in doc.getElementsByTagName('path')]
 doc.unlink()
