ee86409d238c4192290b4f21f11aaea8bab6d64f
jan438 <jan438@users.noreply.github.com>
2026-03-17 09:06:41 +0100

    germany added

--- MotoGPCircuits2026.py
+++ MotoGPCircuits2026.py
@@ -201,7 +201,7 @@ renderPDF.draw(scaleSVG("Wiki/WorldMap.svg", worldmapscale), my_canvas, worldmap
 date_y = 100
 
 for i in range(len(circuitsdata)):
-    if i == 7:
+    if i == 8:
         break
     rce = lookupraceevent(circuitsdata[i][0])
     day = str(rce.day)
