28ddd5877ca7c640d1830eb3f2548ab61314afea
jan438 <jan438@users.noreply.github.com>
2026-03-26 09:35:21 +0100

    print added

--- ProcessSVGfile.py
+++ ProcessSVGfile.py
@@ -16,6 +16,9 @@ def flip_svg_path_vertically(input_svg, output_svg, pathid):
     try:
         circuit = ET.parse(input_svg)
         root = circuit.getroot()
+        nsmap = {'svg': 'http://www.w3.org/2000/svg'}
+        xpaths = root.findall('.//svg:path', namespaces=nsmap)
+        print(xpaths)
         svg_height = root.get("height")
         measurement = svg_height[len(svg_height) - 2:]
         if measurement == "mm" or measurement == "cm" or measurement == "in" or measurement == "px" or measurement == "pt":
