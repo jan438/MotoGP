0447fa546c2e9429746441bca736d770818e9f45
jan438 <jan438@users.noreply.github.com>
2026-03-27 09:01:58 +0100

    style adtr added

--- ProcessSVGfile.py
+++ ProcessSVGfile.py
@@ -14,7 +14,8 @@ def flip_svg_path_vertically(input_svg, output_svg, pathid):
                 print ("tag", "tag", tag.tag, "text", tag.text)
         nsmap = {'svg': 'http://www.w3.org/2000/svg'}
         for path_elem in root.findall('.//svg:path', nsmap):
-            print("path", path_elem, "style", path_elem.get("style"))
+            style_attr = path_elem.get("style")
+            print("path", path_elem, "style", style_attr)
         svg_height = root.get("height")
         measurement = svg_height[len(svg_height) - 2:]
         if measurement == "mm" or measurement == "cm" or measurement == "in" or measurement == "px" or measurement == "pt":
