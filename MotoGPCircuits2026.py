c9f908064122323fcf2632b4b69153380b4ca552
jan438 <jan438@users.noreply.github.com>
2026-03-29 15:06:37 +0200

    starttime added

--- MotoGPCircuits2026.py
+++ MotoGPCircuits2026.py
@@ -224,6 +224,7 @@ for i in range(len(circuitsdata)):
     my_canvas.drawString(leftmargin + col * colwidth + (colwidth - namewidth) / 2, circuit_y - 20, displayname)
     my_canvas.drawString(leftmargin + col * colwidth, circuit_y + date_y, day)
     my_canvas.drawString(leftmargin + col * colwidth + 20, circuit_y + date_y, month)
+    my_canvas.drawString(leftmargin + col * colwidth + 30, circuit_y + date_y, starttime)
     drawing = scaleSVG('Wiki/location.svg', 0.07)
     renderPDF.draw(drawing, my_canvas, leftmargin + col * colwidth, circuit_y + location_y)
     my_canvas.drawString(leftmargin + col * colwidth + 20, circuit_y + location_y + 6, location)
